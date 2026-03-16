4a52678b0dc1adc07777c3574c5d8bcf29b8c5ce
Apple MacBook Pro <applemacbookpro@Apples-MacBook-Pro.local>
2026-03-16 22:47:00 +0100

    action fix

---
+++ version.js
@@ -0,0 +1,13 @@
+{
+  "version": "1.0.1",
+  "required": false,
+  "notes": "Bug fixes and performance improvements",
+  "platforms": {
+    "win32-x64": {
+      "file_name": "CRM_Motosing-win32-x64.zip",
+      "drive_file_id": "YOUR_GOOGLE_DRIVE_FILE_ID",
+      "size": 4000000000,
+      "sha256": "YOUR_SHA256_HASH"
+    }
+  }
+}
